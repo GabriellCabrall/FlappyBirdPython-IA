f17872a5a49d4635c296506a7a8219cd78929b16
gabriel cabral <gabrielbielcabral@gmail.com>
2023-06-30 18:17:38 -0300

    Criando movimentação dos objetos na tela

--- FlappyBird.py
+++ FlappyBird.py
@@ -184,6 +184,7 @@ def main():
     while rodando:
         relogio.tick(30)
 
+        # interação com o usuário
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 rodando = False
@@ -193,5 +194,33 @@ def main():
                 if evento.key == pygame.K_SPACE:
                     for passaro in passaros:
                         passaro.pular()
+        # movendo os itens
+        for passaro in passaros:
+            passaro.mover()
+        chao.mover()
+
+        adicionar_cano = False
+        remover_canos = []
+        for cano in canos:
+            for i, passaro in enumerate(passaros):
+                if cano.colidir(passaro):
+                    passaros.pop(i)
+                if not cano.passou and passaro.x > cano.x:
+                    cano.passou = True
+                    adicionar_cano = True
+            cano.mover()
+            if cano.x + cano.CANO_TOPO.get_width() < 0:
+                remover_canos.append(cano)
+
+        if adicionar_cano:
+            pontos += 1
+            canos.append(Cano(600))
+
+        for cano in remover_canos:
+            canos.remove(cano)
+
+        for i, passaros in enumerate(passaros):
+            if (passaro.y + passaro.imagem.get_height()) > chao.y or passaro.y < 0:
+                passaros.pop(i)
         
         desenhar_tela(tela, passaros, canos, chao, pontos)
