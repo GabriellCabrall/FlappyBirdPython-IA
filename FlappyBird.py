bce0d0c99b4ac1e0cea6975b42d636fe9f6fdc90
Gabriel da Silva Cabral <98562652+GabriellCabrall@users.noreply.github.com>
2023-06-30 13:29:39 -0300

    Criando classe do chão e função de movimento

--- FlappyBird.py
+++ FlappyBird.py
@@ -136,4 +136,24 @@ class Cano():
 
 
 class Chao():
-    pass
+    VELOCIDADE = 5
+    LARGURA = IMAGEM_CHAO.get_width()
+    IMAGEM = IMAGEM_CHAO
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.LARGURA
+
+    def mover(self):
+        self.x1 -= self.VELOCIDADE
+        self.x2 -= self.VELOCIDADE
+
+        if self.x1 + self.LARGURA < 0:
+            self.x1 = self.x1 + self.LARGURA
+        if self.x2 + self.LARGURA < 0:
+            self.x2 = self.x2 + self.LARGURA
+
+    def desenhar(self, tela):
+        tela.blit(self.IMAGEM, (self.x1, self.y))
+        tela.blit(self.IMAGEM, (self.x2, self.pos_base))
