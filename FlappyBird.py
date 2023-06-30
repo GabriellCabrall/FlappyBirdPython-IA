50253133d270f820ef6fb5ae7cb42c1c3b7680f9
gabriel cabral <gabrielbielcabral@gmail.com>
2023-06-30 17:59:19 -0300

    Criando função main, loop de rodar o jogo e interação do usuário

--- FlappyBird.py
+++ FlappyBird.py
@@ -170,3 +170,28 @@ def desenhar_tela(tela, passaros, canos, chao, pontos):
     tela.blit(texto, (TELA_LARGURA - 10 - texto.get_width(), 10))
     chao.desenhar(tela)
     pygame.display.update()
+
+
+def main():
+    passaros = [Passaro(230, 350)]
+    chao = Chao(730)
+    canos = [Cano(700)]
+    tela = pygame.display.set_mode((TELA_LARGURA, TELA_ALTURA))
+    pontos = 0
+    relogio = pygame.time.Clock()
+
+    rodando = True
+    while rodando:
+        relogio.tick(30)
+
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                rodando = False
+                pygame.quit()
+                quit()
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_SPACE:
+                    for passaro in passaros:
+                        passaro.pular()
+        
+        desenhar_tela(tela, passaros, canos, chao, pontos)
