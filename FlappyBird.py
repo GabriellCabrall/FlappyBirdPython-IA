46b4711d11f35df66866698557bb6bf72df2ea59
Gabriel da Silva Cabral <98562652+GabriellCabrall@users.noreply.github.com>
2023-06-30 11:14:16 -0300

    Criando a classe do cano e definindo as posições do cano

--- FlappyBird.py
+++ FlappyBird.py
@@ -93,7 +93,23 @@ class Passaro():
 
 
 class Cano():
-    pass
+    DISTANCIA = 200
+    VELOCIDADE = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.altura = 0
+        self.pos_topo = 0
+        self.pos_base = 0
+        self.CANO_TOPO =pygame.transform.flip(IMAGEM_CANO, False, True)
+        self.CANO_BASE = IMAGEM_CANO
+        self.passou = False
+        self.definir_altura()
+
+    def definir_altura(self):
+        self.altura = random.randrange(50, 450)
+        self.pos_topo = self.altura - self.CANO_TOPO.get_height()
+        self.pos_base = self.altura + self.DISTANCIA
 
 
 class Chao():
